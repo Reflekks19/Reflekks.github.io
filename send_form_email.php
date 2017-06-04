f9b5eee8b086d0fc134921280dee3e8cf97de5fb
Reflekks <logic.lover.301@gmail.com>
2017-06-03 23:14:18 -0400

    Update send_form_email.php

--- send_form_email.php
+++ send_form_email.php
@@ -2,8 +2,8 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "Your email subject line";
+    $email_to = "logic.lover.301@gmail.com";
+    $email_subject = "Reflekks Gaming Question or Comment";
  
     function died($error) {
         // your error code can go here
